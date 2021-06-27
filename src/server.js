895ce921078312eae5b0a7da7adbe37ee9cc0908
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-06-27 21:57:42 +0700

    init first submission

---
+++ src/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const SongsService = require('./services/SongsService');
+const SongsValidator = require('./validator/songs');
+ 
+const init = async () => {
+  const songsService = new SongsService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+	  validator: SongsValidator,
+    },
+  });
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
---
+++ src/utils/index.js
@@ -0,0 +1,31 @@
+const mapDBToModel = ({ 
+    id,
+    title,
+    performer,
+  }) => ({
+    id,
+    title,
+    performer,
+  });
+
+const mapDBToModelSpecified = ({ 
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    created_at,
+    updated_at,
+  }) => ({
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    insertedAt: created_at,
+    updatedAt: updated_at,
+  });
+
+module.exports = { mapDBToModel, mapDBToModelSpecified };
